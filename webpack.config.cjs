09bb94af2fea2861b955d8468e78d824e21eb10a
Kaley Main <kaleypoole17@gmail.com>
2022-10-31 19:57:09 +1100

    Initial commit

---
+++ webpack.config.cjs
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: [/node_modules/, /tests/],
+      },
+    ],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  experiments: {
+    topLevelAwait: true,
+  },
+};
