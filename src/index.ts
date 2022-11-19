2a7ad2a3c08a4426c78230faee6d0c46e1f08a44
Kaley Main <kaleypoole17@gmail.com>
2022-11-19 11:24:33 +1100

    Add background and render loop

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,49 @@
-import { Application, Sprite, Assets } from 'pixi.js';
+import { Application, Sprite, Assets } from "pixi.js";
+import Game from "../lib/game";
+import update from "./update";
+
+const backgroundWidth = 721;
+const backgroundHeight = 726;
 
 // The application will create a renderer using WebGL, if possible,
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container
-const app = new Application();
+const app = new Application({
+  width: backgroundWidth,
+  height: backgroundHeight,
+});
 
 // The application will create a canvas element for you that you
 // can then insert into the DOM
 document.body.appendChild(app.view as any);
 
 // load the texture we need
-const texture = await Assets.load('bunny.png');
+const cityTexture = await Assets.load("data/city1.png");
 
-// This creates a texture from a 'bunny.png' image
-const bunny = new Sprite(texture);
+// Load the background
+const background = await Assets.load("data/back.jpg");
 
-// Setup the position of the bunny
-bunny.x = app.renderer.width / 2;
-bunny.y = app.renderer.height / 2;
+const game = new Game();
 
-// Rotate around the center
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
+// This creates a texture from the background image.
+const backgroundTex = new Sprite(background);
+const city = new Sprite(cityTexture);
 
-// Add the bunny to the scene we are building
-app.stage.addChild(bunny);
+// Setup the position of the background.
+backgroundTex.x = 0;
+backgroundTex.y = 0;
+
+// Setup the position of the city.
+city.x = app.view.width / 2 - city.width / 2;
+city.y = app.view.height / 2 - city.height / 2;
+
+// Add the background to the scene we are building
+app.stage.addChild(backgroundTex);
+
+// Add the city on top.
+app.stage.addChild(city);
 
 // Listen for frame updates
-app.ticker.add(() => {
-    // each frame we spin the bunny around a bit
-    bunny.rotation += 0.01;
+app.ticker.add((deltaMs) => {
+  update(deltaMs, game);
 });
---
+++ src/update.ts
@@ -0,0 +1,13 @@
+import Game from "../lib/game";
+
+/**
+ * Updates the game state on a render loop cycle. Called every cycle.
+ * @param deltaMs The amount of milliseconds elapsed since the last loop cycle.
+ * @param game A reference to the game instance.
+ */
+export default function update(deltaMs: number, game: Game) {
+  // Skip updates if the game is paused, or if there is no time available.
+  if (game.isPaused || deltaMs <= 0) {
+    return;
+  }
+}
