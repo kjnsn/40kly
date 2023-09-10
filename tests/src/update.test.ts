14b41f47cdff24bb6a77781a62e7d2f2959ded1f
Kaley Main <kaleypoole17@gmail.com>
2023-09-10 10:47:40 +1000

    Switch from tsc to swc

--- tests/src/update.test.ts
+++ tests/src/update.test.ts
@@ -1,17 +1,12 @@
 import assert from "assert";
-import { beforeEach, describe, it } from "mocha";
 import Game from "../../lib/game";
 import update from "../../src/update";
 import store from "../../lib/state/store";
 import { FakeResources } from "../../src/resources";
 
 describe("update", () => {
-  var game = new Game(store);
+  const game = new Game(store);
   const resources = new FakeResources();
-
-  beforeEach(() => {
-    game = new Game(store);
-  });
 
   it("increments the update counter", () => {
     assert.equal(game.updateCounter, 0);
